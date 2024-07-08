6f6edbe134e2efd5b8461ce6da41425a37d95dfc
L1uTongwei <1347277058@qq.com>
2024-07-08 21:02:59 +0800

    init

---
+++ firmware/Serial.h
@@ -0,0 +1,101 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+#include "reg8051.h"
+#include <stdint.h>
+#include <string.h>
+
+struct HexMeta{
+    uint8_t byte_count;
+    uint16_t method;
+    uint8_t slot;
+    uint8_t checksum;
+};
+extern struct HexMeta failed;
+extern struct HexMeta returned;
+extern __xdata uint8_t SerialBuffer[0x100];
+extern __xdata uint8_t OutputBuffer[0x100];
+
+void SerialInit(void){ //9600bps
+    SCON = 0x50;
+	TMOD &= 0x0F;
+	TL1 = 0x8F;
+	TH1 = 0xFD;
+	ET1 = 0;
+	TR1 = 1;
+}
+
+void writeByte(uint8_t dat){
+    if((dat >> 4) > 10) SBUF = (dat >> 4) - 10 + 'A';
+    else SBUF = (dat >> 4) + '0';
+    while(TI);
+    TI = 0;
+    if((dat & 0xF) > 10) SBUF = (dat & 0xF) - 10 + 'A';
+    else SBUF = (dat & 0xF) + '0';
+    while(TI);
+    TI = 0;
+}
+
+int16_t readByte(void){
+    uint8_t datA = 0, datB = 0, result = 0;
+    while(RI);
+    datA = SBUF;
+    while(RI);
+    datB = SBUF;
+    if(datA >= '0' && datA <= '9') result += 16 * (datA - '0');
+    else if(datA >= 'A' && datA <= 'F') result += 16 * (datA - 'A' + 10);
+    else if(datA >= 'a' && datA <= 'f') result += 16 * (datA - 'a' + 10);
+    else return -1;
+    if(datB >= '0' && datB <= '9') result += datB - '0';
+    else if(datB >= 'A' && datB <= 'F') result += datB - 'A' + 10;
+    else if(datB >= 'a' && datB <= 'f') result += datB - 'a' + 10;
+    else return -1;
+    return result;
+}
+
+void SerialSend(uint8_t byte_count, uint16_t method, uint8_t slot){
+    uint8_t sum = 0;
+    SBUF = ':';
+    while(TI);
+    TI = 0;
+    writeByte(byte_count), sum += byte_count;
+    writeByte(method >> 8), sum += (method >> 8);
+    writeByte(method & 0xFF), sum += (method & 0xFF);
+    writeByte(slot), sum += slot;
+    for(uint8_t i = 0; i < byte_count; i++){
+        writeByte(OutputBuffer[i]), sum += OutputBuffer[i];
+    }
+    writeByte(0x100 - sum);
+}
+
+void SerialRecv(void){
+    uint8_t dat = 0, sum = 0;
+    int16_t status = 0;
+    struct HexMeta meta;
+    memset(SerialBuffer, 0, sizeof(SerialBuffer));
+    while(dat != ':'){
+        while(RI);
+        dat = SBUF;
+    }
+    if((status = readByte()) == -1) {returned = failed; return;}
+    meta.byte_count = (uint8_t)status, sum += status;
+    if((status = readByte()) == -1) {returned = failed; return;}
+    meta.method = (status << 8), sum += status;
+    if((status = readByte()) == -1) {returned = failed; return;}
+    meta.method |= status, sum += status;
+    if((status = readByte()) == -1) {returned = failed; return;}
+    meta.slot = (uint8_t)status, sum += status;
+    for(uint8_t i = 0; i < meta.byte_count; i++){
+        if((status = readByte()) == -1){
+            memset(SerialBuffer, 0, sizeof(SerialBuffer));
+            returned = failed;
+            return;
+        }
+        SerialBuffer[i] = (uint8_t)status, sum += status;
+    }
+    if((status = readByte()) == -1) {returned = failed; return;}
+    meta.checksum = (uint8_t)status;
+    if(meta.checksum + sum != 0) {returned = failed; return;}
+    returned = meta;
+}
+
+#endif
---
+++ firmware/main.c
@@ -0,0 +1,156 @@
+#include "reg8051.h"
+#define uECC_CURVE uECC_secp256k1
+#define uECC_ASM uECC_asm_none
+#define uECC_PLATFORM uECC_arch_other
+#define uECC_WORD_SIZE 1
+#include "Serial.h"
+#include "micro-ecc/uECC.h"
+#include "micro-ecc/uECC.c"
+#define BACK_TO_TABLES
+#include "aes256/aes256.h"
+#include "aes256/aes256.c"
+#include "sha-2/sha-256.h"
+#include "sha-2/sha-256.c"
+#include "mt19937ar/mt19937ar.c"
+#include <stdlib.h>
+#define SLOT(x) (FRAM + 0x20 * x)
+
+struct HexMeta failed = {0, 0, 0, 0};
+struct HexMeta returned = {0, 0, 0, 0};
+__xdata uint8_t FRAM[0x2000]; //Please place this first so that its addr = 0x0000
+__xdata uint8_t SerialBuffer[0x100], OutputBuffer[0x100], PublicKeyBuffer[0x40], PrivateKeyBuffer[0x20];
+__xdata uint8_t SharedKeyBuffer[0x20], OtherPublicKeyBuffer[0x40], HashBuffer[0x32];
+__xdata aes256_context AES256Context;
+__xdata struct Sha_256 SHA256Context;
+__xdata unsigned long mt[N];
+long timerCount = 0;
+int16_t openedSlot = -1;
+ 
+void TimerInit(void){
+	TMOD &= 0xF0;
+	TL0 = 0xE8;
+	TH0 = 0xFF;
+	TF0 = 0;
+	TR0 = 1;
+}
+void TimerInterrupt(void) __interrupt(1) {timerCount++;}
+
+int RNG(uint8_t *dest, unsigned size){
+    init_genrand(timerCount);
+    while(size > 4){
+        *(uint32_t*)dest = genrand_int32();
+        dest += 4, size -= 4;
+    }
+    while(size--) *(dest++) = (uint8_t)genrand_int32();
+    return 1;
+}
+
+int main(void){
+    SerialInit();
+    TimerInit();
+    uECC_set_rng(RNG);
+    while(1){
+        SerialRecv();
+        if(returned.method){
+            switch(returned.method){
+                case 0x8708:{ // Create a slot
+                    static uint8_t* __xdata slot;
+                    slot = SLOT(returned.slot);
+                    memset(PrivateKeyBuffer, 0, 0x20);
+                    memset(PublicKeyBuffer, 0, 0x40);
+                    memset(HashBuffer, 0, 0x32);
+                    calc_sha_256(HashBuffer, SerialBuffer, returned.byte_count);
+                    aes256_init(&AES256Context, HashBuffer);
+                    uECC_make_key(PublicKeyBuffer, PrivateKeyBuffer);
+                    memcpy(slot, PrivateKeyBuffer, 0x20);
+                    aes256_encrypt_ecb(&AES256Context, slot);
+                    aes256_done(&AES256Context);
+                    openedSlot = returned.slot;
+                    returned.byte_count = 1;
+                    OutputBuffer[0] = 0;
+                    SerialSend(returned.byte_count, returned.method, returned.slot);
+                    break;
+                }
+                case 0x1c8d:{ // Open a slot
+                    static uint8_t* __xdata slot;
+                    slot = SLOT(returned.slot);
+                    memset(PrivateKeyBuffer, 0, 0x20);
+                    memset(PublicKeyBuffer, 0, 0x40);
+                    memset(HashBuffer, 0, 0x32);
+                    calc_sha_256(HashBuffer, SerialBuffer, returned.byte_count);
+                    aes256_init(&AES256Context, HashBuffer);
+                    memcpy(PrivateKeyBuffer, slot, 0x20);
+                    aes256_decrypt_ecb(&AES256Context, slot);
+                    aes256_done(&AES256Context);
+                    openedSlot = returned.slot;
+                    returned.byte_count = 1;
+                    OutputBuffer[0] = 0;
+                    SerialSend(returned.byte_count, returned.method, returned.slot);
+                    break;
+                }
+                case 0xe6c5: case 0x4acb: case 0x4bd8: case 0xe64a:{ // En/Decrypt with secret/shared
+                    static uint8_t i;
+                    if(openedSlot == -1){
+                        returned.byte_count = 1;
+                        OutputBuffer[0] = 1;
+                        SerialSend(returned.byte_count, returned.method, returned.slot);
+                        break;
+                    }
+                    memcpy(OutputBuffer, SerialBuffer, returned.byte_count);
+                    if(returned.method == 0xe6c5 || returned.method == 0x4acb) aes256_init(&AES256Context, PrivateKeyBuffer);
+                    else{
+                        if(!uECC_valid_public_key(OtherPublicKeyBuffer)){
+                            returned.byte_count = 1;
+                            OutputBuffer[0] = 2;
+                            SerialSend(returned.byte_count, returned.method, returned.slot);
+                            break;
+                        }
+                        uECC_shared_secret(OtherPublicKeyBuffer, PrivateKeyBuffer, SharedKeyBuffer);
+                        aes256_init(&AES256Context, SharedKeyBuffer);
+                    }
+                    for(i = 0; i < 8; i++){
+                        if(0x20 * i > returned.byte_count){
+                            break;
+                        }
+                        if(returned.method == 0xe6c5 || returned.method == 0x4bd8) aes256_encrypt_ecb(&AES256Context, OutputBuffer + 0x20 * i);
+                        else aes256_decrypt_ecb(&AES256Context, OutputBuffer + 0x20 * i);
+                    }
+                    aes256_done(&AES256Context);
+                    returned.byte_count = 0x20 * (i + 1);
+                    SerialSend(returned.byte_count, returned.method, returned.slot);
+                    memset(SharedKeyBuffer, 0, 0x40);
+                    break;
+                }
+                case 0xd720:{ // Get Public Key
+                    if(openedSlot == -1){
+                        returned.byte_count = 1;
+                        OutputBuffer[0] = 1;
+                        SerialSend(returned.byte_count, returned.method, returned.slot);
+                        break;
+                    }
+                    memcpy(OutputBuffer, PublicKeyBuffer, 0x40);
+                    returned.byte_count = 0x40;
+                    SerialSend(returned.byte_count, returned.method, returned.slot);
+                    break;
+                }
+                case 0xd3a2:{ // Clear Other's Public Key
+                    memset(OtherPublicKeyBuffer, 0, 0x40);
+                    returned.byte_count = 1;
+                    OutputBuffer[0] = 0;
+                    SerialSend(returned.byte_count, returned.method, returned.slot);
+                    break;
+                }
+                case 0xdcab:{ // Close a slot
+                    openedSlot = -1;
+                    memset(PrivateKeyBuffer, 0, 0x20);
+                    memset(PublicKeyBuffer, 0, 0x40);
+                    OutputBuffer[0] = 0;
+                    SerialSend(returned.byte_count, returned.method, returned.slot);
+                    break;
+                }
+            }
+            memset(OutputBuffer, 0, 0x100);
+            memset(&returned, 0, sizeof(returned));
+        }
+    }
+}
